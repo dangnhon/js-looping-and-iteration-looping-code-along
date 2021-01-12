39f52daacaa054b23a45a6e35ca498f740e9249e
dangnhon <dang.nhon12345@yahoo.com>
2021-01-11 17:07:44 -0800

    weird decrement stuff

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arrayNames, event) {
+    let messages = [];
+    for (let i = 0; i < arrayNames.length; i++) {
+        messages.push(`Thank you, ${arrayNames[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+function countDown(number) {
+    let i = -1; 
+    while (number > i) {
+        console.log(number --);
+    }
+}
